5515d2119cb1e9e8e159feeaf8510328da97e97e
OlZe <oliverzeidler97@gmail.com>
2020-05-27 20:21:46 +0200

    added @angular/material, @angular/pwa and @angular/flex-layout to project

--- to-do-matrix/src/app/app.module.ts
+++ to-do-matrix/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
