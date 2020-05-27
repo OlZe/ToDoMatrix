02c4620998f25448c6ff3977f488dab82330a495
OlZe <oliverzeidler97@gmail.com>
2020-05-28 01:11:07 +0200

    tasks are now read from and written to localstorage

--- to-do-matrix/src/app/model/tasks.service.ts
+++ to-do-matrix/src/app/model/tasks.service.ts
@@ -1,6 +1,9 @@
 import { Task } from './task.model';
 import { Injectable } from '@angular/core';
 
+
+const TASK_KEY = 'tasks';
+
 @Injectable()
 export class TasksService {
     private tasks: Task[] = [
@@ -10,11 +13,32 @@ export class TasksService {
         { title: 'task4' }
     ];
 
+    constructor() {
+        this.loadTasksFromLocalStorage();
+        window.onstorage = (e: StorageEvent) => {
+            this.loadTasksFromLocalStorage();
+        };
+    }
+
     public getTasks(): Task[] {
         return this.tasks;
     }
 
     public addTask(task: Task) {
         this.tasks.push(task);
+        this.saveTasksToLocalStorage();
+    }
+
+    private loadTasksFromLocalStorage() {
+        try {
+            this.tasks = JSON.parse(localStorage.getItem(TASK_KEY)) ?? [];
+        } catch (error) {
+            this.tasks = [];
+            console.error('Cannot load saved todo items from local storage as data is corrupted!');
+        }
+    }
+
+    private saveTasksToLocalStorage() {
+        localStorage.setItem(TASK_KEY, JSON.stringify(this.tasks));
     }
 }
