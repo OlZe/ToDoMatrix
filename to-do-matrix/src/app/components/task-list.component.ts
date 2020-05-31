19a1614d34a01e50d1789b0654546c73c019df27
OlZe <oliverzeidler97@gmail.com>
2020-05-31 17:39:44 +0200

    task list in overview is now sorted by priority of each task

--- to-do-matrix/src/app/components/task-list.component.ts
+++ to-do-matrix/src/app/components/task-list.component.ts
@@ -6,11 +6,18 @@ import { TasksService } from '../model/tasks.service';
     selector: 'app-task-list',
     templateUrl: './task-list.component.html'
 })
-export class TaskListComponent {
 
+export class TaskListComponent {
     public constructor(private tasksService: TasksService) { }
 
     public getTasks(): Task[] {
-        return this.tasksService.getTasks();
+        return this.tasksService.getTasks().sort(this.sortByPriorityDescending);
+    }
+
+    // Variable instead of function because it needs to be passed as argument
+    private sortByPriorityDescending = (a: Task, b: Task) => this.calcPriorityScore(b) - this.calcPriorityScore(a);
+
+    private calcPriorityScore(task: Task): number {
+        return task.urgency * 2 + task.importance;
     }
 }
